42358a4b535c83a413793e854e0804ec87ea98b5
BarevAp <159822026+BarevAp@users.noreply.github.com>
2024-02-12 23:22:37 +0400

    Update FirstTask.java

--- FirstTask.java
+++ FirstTask.java
@@ -1,24 +1,21 @@
 package PicsArtClone;
 
-import java.sql.Struct;
-import java.util.Arrays;
-
 public class FirstTask {
     public static void main(String[] args) {
         int[][] matrix = {{1, 1, 0, 1, 1,},
                           {1, 1, 0, 1, 0},
-                          {0, 0, 0, 0, 0}};
+                          {0, 0, 1, 0, 0}};
 
         int objects = findObjects(matrix);
         System.out.println(objects);
 
-//        horizontalReversePicture(matrix);
-//        for (int i = 0; i < matrix.length; i++) {
-//            for (int j = 0; j < matrix[i].length; j++) {
-//                System.out.print(matrix[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
+        horizontalReversePicture(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
 
     }
 
